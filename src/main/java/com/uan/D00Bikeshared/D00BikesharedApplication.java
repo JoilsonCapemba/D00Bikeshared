f94250e29adebd4b2c0538e483db41e707357d92
JoilsonCapemba <joilsoncapemba@gmail.com>
2024-05-30 15:08:16 -0300

    Setup do projecto

---
+++ src/main/java/com/uan/D00Bikeshared/D00BikesharedApplication.java
@@ -0,0 +1,13 @@
+package com.uan.D00Bikeshared;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class D00BikesharedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(D00BikesharedApplication.class, args);
+	}
+
+}
