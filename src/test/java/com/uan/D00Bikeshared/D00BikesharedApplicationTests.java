843cf4fd4ed1d4f46598b40fd700605ab1760185
JoilsonCapemba <joilsoncapemba@gmail.com>
2024-05-30 22:20:44 -0300

    recomecando com a configuracao do ws com a bd

---
+++ src/test/java/com/uan/D00Bikeshared/D00BikesharedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uan.D00Bikeshared;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class D00BikesharedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
